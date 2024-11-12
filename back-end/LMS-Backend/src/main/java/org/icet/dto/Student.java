7c13ed03384227ea0457f87b757c756bda957cd2
RuwanDilhara <roshansameera916@gmail.com>
2024-11-12 20:38:56 +0530

    2nd

--- back-end/LMS-Backend/src/main/java/org/icet/dto/Student.java
+++ back-end/LMS-Backend/src/main/java/org/icet/dto/Student.java
@@ -1,6 +1,13 @@
 package org.icet.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
+
+import java.time.LocalDate;
+
 
 @Data
 @AllArgsConstructor
@@ -8,6 +15,20 @@ import lombok.*;
 @ToString
 
 public class Student {
-    private String studentID;
-    private String stringName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studentID;
+    private String fullName;
+    private String userName;
+    private String email;
+    private String phoneNumber;
+    private LocalDate dOB;
+    private String address;
+    private String grade;
+
+    @JsonIgnore
+    private String password;
+
+
+    //private List<Course> coursesEnrolled;
 }
